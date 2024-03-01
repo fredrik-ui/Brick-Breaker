f770fcaf73f0f7f5e41203d4ec174c60f1923b28
Fredrik Kis <fredrik.kis03@gmail.com>
2024-03-01 20:08:11 +0100

    Remove usless code

--- Main.java
+++ Main.java
@@ -1,9 +1,6 @@
-import java.awt.*;
-import java.awt.event.*; 
-
 public class Main{
 
    public static void main(String args[]){
-        MyFrame f = new MyFrame(); 
+        new MyFrame(); 
     }
 }
