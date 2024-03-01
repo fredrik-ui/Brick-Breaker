864248d79209877066ddc8811128d71733f2fedf
Fredrik Kis <fredrik.kis03@gmail.com>
2024-03-01 21:08:42 +0100

    Performance imrpoved with dubble buffering

--- V2/MyFrame.java
+++ V2/MyFrame.java
@@ -1,7 +1,6 @@
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import java.util.Timer;
-import java.util.TimerTask;
 
 public class MyFrame extends Frame {
 
@@ -22,82 +21,19 @@ public class MyFrame extends Frame {
         setVisible(true);
         setBackground(Color.GRAY);
 
-        Timer timer = new Timer();
-
-        TimerTask ballMovementTask = new TimerTask() {
+        Timer timer = new Timer(50, new ActionListener() {
             @Override
-            public void run() {
-                int posY;
-                int posX;
-                // Check if ball collides with the walls
-                int List[] = Collision.check_ball_list(
-                    getWidth(),
-                    getHeight(),
-                    myBall.getRadius(),
-                    myBall.getX(), 
-                    myBall.getY(), 
-                    myBall.getVelocityY(),
-                    myBall.getVelocityX()
-                );
-                posY=List[1];
-                posX=List[0];
-
-                // Check if ball collides with paddle
-                if(
-                    Collision.paddle_ball_colide(
-                        rectX,
-                        rectY,
-                        rectWidth,
-                        rectHeight,
-                        myBall.getRadius(),
-                        myBall.getX(), 
-                        myBall.getY(), 
-                        myBall.getVelocityY(),
-                        myBall.getVelocityX()
-                    ) == (myBall.veleocityY*-1)
-                ){
-                    posY = (myBall.veleocityY*-1);
-                }
-
-                myBall.veleocityX = posX;
-                myBall.veleocityY = posY;
-                myBall.updatePosition(myBall.getX() - posX, myBall.getY() + posY);
-
-                // int posY = Collision.check_ball(getWidth(),getHeight(),myBall.getWidth(),myBall.getHeight(),myBall.getX(), myBall.getY(), myBall.getVelocityY(),myBall.getVelocityX());
-                // myBall.updatePosition(myBall.getX(), myBall.getY() + posY);
-                repaint(); // Repaint the frame to reflect the changes
+            public void actionPerformed(ActionEvent e) {
+                updateBallPosition();
+                repaint();
             }
-        };
-
-        // Schedule the TimerTask
-        timer.scheduleAtFixedRate(ballMovementTask, 0, 50);
+        });
+        timer.start();
 
         addKeyListener(new KeyAdapter() {
             @Override
             public void keyPressed(KeyEvent e) {
-                switch (e.getKeyCode()) {
-                    case KeyEvent.VK_UP:
-                        rectY -= 5; // Move the rectangle up
-                        break;
-                    case KeyEvent.VK_RIGHT:
-                        if (Collision.check_paddel(rectX, rectWidth, windowWidth) == 2)
-                            rectX += 0;
-                        else {
-                            rectX += 5; // Move the rectangle right
-                        }
-                        break;
-                    case KeyEvent.VK_LEFT:
-                        if (Collision.check_paddel(rectX, rectWidth, windowWidth) == 1)
-                            rectX += 0;
-                        else {
-                            rectX -= 5; // Move the rectangle right
-                        }
-                        break;
-                    case KeyEvent.VK_DOWN:
-                        rectY += 5; // Move the rectangle down
-                        break;
-                }
-                repaint(); // Repaint the frame to reflect the changes
+                KeyPress(e);
             }
         });
 
@@ -109,20 +45,90 @@ public class MyFrame extends Frame {
         });
     }
 
-    public void paint(Graphics g) {
-        // Set color to blue
-        myBall.paint(g);
-        g.setColor(Color.BLUE);
-        int x =0;
-        while(x< windowWidth) {
-            g.fillRect(10+x, 300, 100, 50);
-            x += 110;
+    public void updateBallPosition(){
+        int posY;
+        int posX;
+        // Check if ball collides with the walls
+        int List[] = Collision.check_ball_list(
+            getWidth(),
+            getHeight(),
+            myBall.getRadius(),
+            myBall.getX(), 
+            myBall.getY(), 
+            myBall.getVelocityY(),
+            myBall.getVelocityX()
+        );
+        posY=List[1];
+        posX=List[0];
+
+        // Check if ball collides with paddle
+        if(
+            Collision.paddle_ball_colide(
+                rectX,
+                rectY,
+                rectWidth,
+                rectHeight,
+                myBall.getRadius(),
+                myBall.getX(), 
+                myBall.getY(), 
+                myBall.getVelocityY(),
+                myBall.getVelocityX()
+            ) == (myBall.veleocityY*-1)
+        ){
+            posY = (myBall.veleocityY*-1);
         }
 
-        // g.fillOval(50,50,20,20);
+        myBall.veleocityX = posX;
+        myBall.veleocityY = posY;
+        myBall.updatePosition(myBall.getX() - posX, myBall.getY() + posY);
 
-        // Draw rectangle with the specified parameters
-        // (x, y, width, height)
-        g.fillRect(rectX, rectY, rectWidth, rectHeight);
+    }
+
+    public void KeyPress(KeyEvent e){
+        switch (e.getKeyCode()) {
+            case KeyEvent.VK_UP:
+                rectY -= 5; // Move the rectangle up
+                break;
+            case KeyEvent.VK_RIGHT:
+                if (Collision.check_paddel(rectX, rectWidth, windowWidth) == 2)
+                    rectX += 0;
+                else {
+                    rectX += 5; // Move the rectangle right
+                }
+                break;
+            case KeyEvent.VK_LEFT:
+                if (Collision.check_paddel(rectX, rectWidth, windowWidth) == 1)
+                    rectX += 0;
+                else {
+                    rectX -= 5; // Move the rectangle right
+                }
+                break;
+            case KeyEvent.VK_DOWN:
+                rectY += 5; // Move the rectangle down
+                break;
+        }
+        repaint(); // Repaint the frame to reflect the changes
+    }
+
+
+
+    public void paint(Graphics g) {
+        // Create an off-screen buffer
+        // Based on this link: https://www.ecst.csuchico.edu/~akeuneke/classes/csciOOP/double-buffering.html
+        Image offscreen = createImage(getWidth(), getHeight());
+        Graphics offscreenGraphics = offscreen.getGraphics();
+    
+        // Draw on the off-screen buffer
+        myBall.paint(offscreenGraphics);
+        offscreenGraphics.setColor(Color.BLUE);
+        int x = 0;
+        while (x < windowWidth) {
+            offscreenGraphics.fillRect(10 + x, 300, 100, 50);
+            x += 110;
+        }
+        offscreenGraphics.fillRect(rectX, rectY, rectWidth, rectHeight);
+    
+        // Draw the off-screen buffer onto the screen
+        g.drawImage(offscreen, 0, 0, this);
     }
 }
--- V2/ball.java
+++ V2/ball.java
@@ -34,4 +34,5 @@ public class ball{
         g.setColor(Color.red);
         g.fillOval(x,y,radius,radius);
     }
+
 }
