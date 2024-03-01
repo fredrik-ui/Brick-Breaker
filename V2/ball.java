8fca7b162260e4ec5a0890acdf97bcb92d971ee9
Fredrik Kis <fredrik.kis03@gmail.com>
2024-03-01 20:31:46 +0100

    Going to improve performance

---
+++ V2/ball.java
@@ -0,0 +1,37 @@
+import java.awt.*;
+
+public class ball{
+    
+    public int veleocityY = 10;
+    public int veleocityX = 2;
+    private int x = 50;
+    private int y = 600;
+    private int radius = 20;
+
+    public void updatePosition(int newX, int newY) {
+        this.x = newX;
+        this.y = newY;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+    public int getVelocityY() {
+        return veleocityY;
+    }
+    public int getVelocityX() {
+        return veleocityX;
+    }
+    public void paint(Graphics g) {
+        g.setColor(Color.red);
+        g.fillOval(x,y,radius,radius);
+    }
+}
